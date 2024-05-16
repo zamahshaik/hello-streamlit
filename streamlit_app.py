f33dd1ddf84fcc352c7c6be793ed042721188f85
zamahshaik <32522307+zamahshaik@users.noreply.github.com>
2024-05-16 15:14:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.set_page_config(page_title = "MEI Chatbot", page_icon = ":robot:", layout = "wide")
+
+# -- Header Section --
+
+st.subheader("Hi, I am a chatbot :wave:")
+st.title("I help find PDFs")
+st.write("More functionality coming up here!")
