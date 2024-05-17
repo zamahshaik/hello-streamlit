c111247dd247165d9315697ae12adb906cc22a2e
zamahshaik <32522307+zamahshaik@users.noreply.github.com>
2024-05-17 14:53:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ st.set_page_config(page_title = "MEI Chatbot", page_icon = ":robot:", layout = "
 st.subheader("Hi, I am a chatbot :wave:")
 st.title("I help find PDFs")
 st.write("More functionality coming up here!")
+pdfname = input("Enter the Purchase Order to search for: ")
+print(pdfname)
